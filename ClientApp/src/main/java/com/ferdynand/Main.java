be3b6d6682ae7b119cbe2b83dbc95ccd018c0e4c
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-01-18 22:31:10 +0100

    adding .gitignore and basic packages and classes

---
+++ ClientApp/src/main/java/com/ferdynand/Main.java
@@ -0,0 +1,28 @@
+package main.java.com.ferdynand;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private final FXMLLoader loader = new FXMLLoader(getClass().getResource("views/mainWindow.fxml"));
+
+    @Override
+    public void start(Stage primaryStage){
+        try {
+            Parent root = loader.load();
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ ClientApp/src/test/java/com/ferdynand/MainTest.java
@@ -0,0 +1,16 @@
+package test.java.com.ferdynand;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void start() {
+    }
+
+    @org.testng.annotations.Test
+    void main() {
+    }
+}
